9b3cc6684e6073dc022d5779c9ca3b02efce3dcb
Marea <mtteoiaki@gmail.com>
2020-02-20 21:17:25 +1300

    Initial commit

---
+++ app/src/main/java/com/beeniara/backgroundmusicservice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.beeniara.backgroundmusicservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
